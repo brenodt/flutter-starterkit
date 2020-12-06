0e04729d9a7d146f8ef21e38e8f40a77d0df650c
Breno Teodoro <54450520+brenodt@users.noreply.github.com>
2020-12-06 19:02:33 -0300

    Set observer in main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
 import 'localization.dart';
 import 'navigation.dart';
+import 'src/services/app_analytics.dart';
 import 'src/services/connectivity_service.dart';
 
 void main() {
@@ -12,8 +15,9 @@ void main() {
     MultiProvider(
       providers: [
         Provider<ConnectionStatus>(create: (_) => ConnectionStatus()),
+        Provider<AppAnalytics>(create: (_) => AppAnalytics()),
       ],
-      child: MyApp(),
+      child: App(),
     ),
   );
 }
@@ -24,6 +28,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       initialRoute: Routes.home,
       onGenerateRoute: Routes.onGenerateRoute,
+      navigatorObservers: [Provider.of<AppAnalytics>(context).observer],
       localizationsDelegates: [
         AppLocalization.delegate,
         GlobalMaterialLocalizations.delegate,
